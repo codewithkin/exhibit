2f16c19851a58f072233197c6fbc4c38dc138471
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-01-03 09:06:15 +0200

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@exhibit/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
