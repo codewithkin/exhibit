a3721cf574f6a63d4e43e2a9954394ffeef8b0dc
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-01-03 19:25:12 +0200

    fix: configure Prisma 7 with pg adapter and Pool

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,13 +1,11 @@
 import { env } from "@exhibit/env/server";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 import { PrismaClient } from "../prisma/generated/client";
 
-const adapter = new PrismaPg({
-  connectionString: env.DATABASE_URL,
-});
-
-// const ACCELERATE_URL = env.ACCELERATE_URL;
+const pool = new Pool({ connectionString: env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({ adapter });
 
